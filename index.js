11fe65a609c5bef40c5020d1f8b5fb02b3ea3215
Bildan Jauhary <bildanjauhary@gmail.com>
2026-05-13 10:37:28 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const PI = 3.14
+let r = 100
+
+const luas = PI * r * r
+const keliling = 2 * PI * r
+
+console.log(`Luas dari lingkaran = ${luas}`)
+console.log(`Keliling dari lingkaran = ${keliling}`)
